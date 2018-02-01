9592effa9e89428bda87e86a61b6b6661bab63a7
王玲波 <454949876@qq.com>
2018-02-01 10:57:22 +0800

    API地址变更

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   	NODE_ENV: '"production"',
   	ENV_CONFIG: '"prod"',
-  	BASE_API: '"http://116.62.150.251:1234/"'
+  	BASE_API: '"http://www.mdguanjia.com/bop/"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"sit"',
-	BASE_API: '"http://116.62.150.251:1234/"'
+	/*BASE_API: '"http://116.62.150.251:1234/"'*/
+	BASE_API: '"http://dev.mdguanjia.com/bop/"'
 }
