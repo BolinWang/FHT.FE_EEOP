fcbeaa7d19c4d1f5bfe27dc0b4d395c164f1f273
王玲波 <454949876@qq.com>
2018-03-08 13:33:11 +0800

    切换至http服务

--- config/sit.env.js
+++ config/sit.env.js
@@ -2,5 +2,5 @@ module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"sit"',
 	/*BASE_API: '"http://116.62.150.251:1234/"'*/
-	BASE_API: '"http://dev.mdguanjia.com/bop/"'
+	BASE_API: '"https://dev.mdguanjia.com/bop/"'
 }
