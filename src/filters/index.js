0e6eb6ae6ff1e4f35b1c87687e416a66f5787714
王玲波 <454949876@qq.com>
2018-04-08 15:37:03 +0800

    千位分割

--- src/filters/index.js
+++ src/filters/index.js
@@ -102,6 +102,8 @@ export function html2Text(val) {
   return div.textContent || div.innerText
 }
 
+/* 千位分割 */
 export function toThousandslsFilter(num) {
   return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
 }
+
