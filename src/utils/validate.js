1df853cbd60387347ad4f1859446cf96f327b9dd
chenxing_ <709329364@qq.com>
2018-04-20 21:39:18 +0800

    手机验证

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -6,8 +6,8 @@ export function isWscnEmail(str) {
 
 /* 手机号 */
 export function validateMobile(str) {
-  const reg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0-9]{1})|(15[0-3]{1})|(15[4-9]{1})|(18[0-9]{1})|(199))+\d{8})$/;
-  return reg.test(str);
+  const reg = /^1[23456789]\d{9}$/
+  return reg.test(str)
 }
 
 /* 合法uri*/
