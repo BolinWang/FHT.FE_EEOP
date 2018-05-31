9f6503206de34e76a98c87532a2efdd053eb3bf0
王玲波 <454949876@qq.com>
2018-06-01 01:57:00 +0800

    ### *柏林* 测试路由history模式

--- config/index.js
+++ config/index.js
@@ -32,7 +32,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     productionSourceMap: false,
     devtool: '#source-map',
     productionGzip: false,
