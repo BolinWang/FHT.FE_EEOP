2ef0506dd1ecc21c247cc98232eea9a8fc16205c
王玲波 <454949876@qq.com>
2018-07-12 16:34:32 +0800

    ### *柏林* OTA界面提交   等待接口文档

--- src/views/houseManage/authorize.vue
+++ src/views/houseManage/authorize.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: FT.FE.Bolin
+ * @Date: 2018-07-11 13:49:21
+ * @Last Modified by: FT.FE.Bolin
+ * @Last Modified time: 2018-07-12 16:21:23
+ */
+
 <template>
   <div class="app-container">
     <el-form class="model-search clearfix" :inline="true" size="small">
@@ -15,33 +22,68 @@
       ref="refGridUnit"
       :columns="colModels"
       :formOptions="searchParams"
-      :showPagination="false"
       :url="url"
       :dataMethod="method"
       :height="tableHeight"
       :showExpand="true"
-      :expandType="`table`"
       :expandColums="colModels">
       <template slot="expandTable" slot-scope="scope">
-        <el-table :data="scope.row || []" style="width: 100%"
-          border
+        <el-table :data="[{
+          organizationName: '柏林',
+          status: 1
+        }]" style="width: 100%"
           :max-height="300"
           size="small"
           :header-row-class-name="`expandHeader`">
-          <el-table-column v-for="(item, index) in colModels" :label="item.label" :key="index" :prop="item.prop">
+          <el-table-column v-for="(column, index) in expand_colModels" :label="column.label" :key="index" :prop="column.prop">
+            <template slot-scope="scope">
+              <span v-if="column.slotName">
+                <el-button v-if="scope.row.status === 0" @click="handleSetting(scope.row)" type="text" size="small">申请开通</el-button>
+                <el-button v-else type="text" disabled size="small">已开通</el-button>
+              </span>
+              <span v-else>
+                {{ column.render ? column.render(scope.row) : scope.row[column.prop] }}
+              </span>
+            </template>
           </el-table-column>
         </el-table>
       </template>
       <template slot="handle" slot-scope="scope">
-        <el-button v-if="scope.row.status === 0" type="primary" icon="el-icon-setting" size="small"
-          @click="handleSetting(scope.row)">
-          申请开通
-        </el-button>
-        <el-button v-else type="success" disabled icon="el-icon-success" size="small">
-          已开通
-        </el-button>
+        <el-button v-if="scope.row.status === 0" @click="handleSetting(scope.row)" type="text" size="small">申请开通</el-button>
+        <el-button v-else type="text" disabled size="small">已开通</el-button>
       </template>
     </GridUnit>
+    <!-- 申请开通 -->
+    <div class="dialog_apply">
+      <el-dialog title="申请开通" :visible.sync="layer_showApply" @close="dialogClose">
+        <el-steps :active="active_step" align-center finish-status="success" style="margin-bottom: 30px;">
+          <el-step title="填写闲鱼昵称" description="打开闲鱼APP，点击【我的】，用户名即为闲鱼昵称，闲鱼昵称需要进行实名认证才可同步房源（闲鱼APP-我的-头像-实名认证）"></el-step>
+          <el-step title="扫码授权" description="打开闲鱼APP进行扫码授权"></el-step>
+        </el-steps>
+        <div class="text-center" style="display: flex; justify-content: center;" v-if="active_step === 0">
+          <el-form size="small" status-icon :rules="rules" ref="dataForm" :model="temp" style="width: 400px;">
+            <el-form-item prop="name">
+              <el-input placeholder="请输入闲鱼昵称" v-model="temp.name" clearable size="small">
+                <template slot="prepend">闲鱼昵称</template>
+              </el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="text-center" v-else style="display: flex; justify-content: center;">
+          <el-card :body-style="{ padding: '0px' }" style="width: 500px;">
+            <img src="../../assets/banner.jpg" style="display: block; width: 100%;">
+            <div style="padding: 14px;">
+              <span>请用闲鱼APP进行扫码授权</span>
+            </div>
+          </el-card>
+        </div>
+        <div slot="footer" class="dialog-footer text-center">
+          <el-button v-if="active_step === 0" type="primary" @click="nextStep" size="small">下一步</el-button>
+          <el-button v-if="active_step === 1" @click="active_step = 0" size="small">上一步</el-button>
+          <el-button v-if="active_step === 1" type="primary" @click="saveApplyInfo" size="small">确认授权</el-button>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 <script>
@@ -61,43 +103,47 @@ export default {
         organizationName: '',
         mobile: ''
       },
-      cityList: [],
       colModels: [
         { prop: 'organizationName', label: '组织名称' },
         { prop: 'status', label: '组织状态' },
         { prop: 'status', label: '姓名' },
         { prop: 'status', label: '手机号' },
         { prop: 'status', label: '用户类型' },
-        { prop: 'status', label: '账号状态' },
         { prop: 'status', label: '闲鱼账号' },
-        { label: '操作', slotName: 'handle', width: 150, align: 'center' },
+        { label: '操作', slotName: 'handle', width: 150 },
+        { prop: 'status', label: '闲鱼昵称' }
+      ],
+      expand_colModels: [
+        { prop: 'status', label: '姓名' },
+        { prop: 'status', label: '手机号' },
+        { prop: 'status', label: '用户类型' },
+        { prop: 'status', label: '闲鱼账号' },
+        { label: '操作', slotName: 'handle', width: 150 },
         { prop: 'status', label: '闲鱼昵称' }
       ],
       tableHeight: 300,
       url: hotRecommendApi.defaultOptions.requestUrl,
       method: hotRecommendApi.defaultOptions.method,
       rules: {
-        data: [
-          { required: true, message: '请输入搜索词', trigger: 'blur' }
-        ],
-        cityId: [
-          { required: true, message: '请选择城市', trigger: 'change' }
+        name: [
+          { required: true, message: '请填写闲鱼昵称', trigger: 'blur' }
         ]
       },
-      temp: {},
-      layer_showInfo: false
+      temp: {
+        name: ''
+      },
+      active_step: 0,
+      layer_showApply: false
     }
   },
   created () {
-    appIconApi.cityList().then(response => {
-      this.cityList = response.data
-    })
+
   },
   mounted() {
     /* 表格高度控制 */
    this.$nextTick(() => {
       const offsetTop = this.$refs.refGridUnit.$el.offsetTop || 140
-      const pagenationH = 5
+      const pagenationH = 64
       const containerPadding = 20
       let temp_height = document.body.clientHeight - offsetTop - pagenationH - containerPadding
       this.tableHeight = temp_height > 300 ? temp_height : 300
@@ -118,14 +164,48 @@ export default {
     }
   },
   methods: {
+    // 查询
     searchParam(type) {
       if (type === 'clear') {
         this.searchParams = {}
       }
       this.$refs.refGridUnit.searchHandler()
     },
+    // 申请开通
     handleSetting(row) {
+      this.layer_showApply = true
+    },
+    // 下一步
+    nextStep() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.active_step = 1
+        }
+      })
+    },
+    // 确认授权
+    saveApplyInfo() {
+      return false
+      hotRecommendApi.add(ObjectMap({
 
+      })).then(response => {
+        this.layer_showApply = false
+        this.searchParam()
+        this.$notify({
+          title: '成功',
+          message: '授权成功',
+          type: 'success',
+          duration: 2000
+        })
+      })
+    },
+    dialogClose() {
+      this.temp = {}
+      this.active_step = 0
+      if (this.$refs.dataForm) {
+        this.$refs.dataForm.resetFields()
+      }
+      this.layer_showApply = false
     }
   }
 }
@@ -133,5 +213,8 @@ export default {
 <style rel="stylesheet/scss" lang="scss">
 .model-table {
   margin-bottom: 0;
+}
+.dialog_apply .el-step__description {
+  color: #c0c4cc !important;
 }
 </style>
---
+++ src/views/houseManage/houseSync.vue
@@ -0,0 +1,133 @@
+/*
+ * @Author: FT.FE.Bolin
+ * @Date: 2018-07-11 13:49:21
+ * @Last Modified by: FT.FE.Bolin
+ * @Last Modified time: 2018-07-12 16:31:20
+ */
+
+ <template>
+  <div class="app-container">
+    <el-tabs v-model="activeName" type="border-card">
+      <el-tab-pane v-for="(item,index) in tabMapOptions" :label="item" :key='index' :name="item">
+        <el-form class="model-search clearfix" :inline="true" size="small">
+          <div>
+            <el-select size="small" v-model="searchParams.type" filterable clearable placeholder="房间状态" class="item-select">
+              <el-option label="已出租" :value="1"></el-option>
+              <el-option label="未出租" :value="2"></el-option>
+            </el-select>
+            <el-select size="small" v-model="searchParams.type" filterable clearable placeholder="房间类型" class="item-select filter-item">
+              <el-option label="普通" :value="1"></el-option>
+              <el-option label="金融" :value="2"></el-option>
+              <el-option label="飞虎队" :value="3"></el-option>
+            </el-select>
+            <el-select size="small" v-model="searchParams.type" filterable clearable placeholder="发布状态" class="item-select filter-item">
+              <el-option label="已发布" :value="2"></el-option>
+              <el-option label="未发布" :value="3"></el-option>
+            </el-select>
+            <el-button size="small" type="primary" icon="el-icon-search" @click="searchParam" class="filter-item">查询</el-button>
+            <el-button size="small" icon="el-icon-remove-outline" @click="searchParam('clear')" class="filter-item">清空</el-button>
+          </div>
+          <div style="margin-top: 10px;">
+            <el-input size="small" v-model="searchParams.organizationName" clearable placeholder="组织名称" style="width:150px;"></el-input>
+            <el-input size="small" v-model="searchParams.mobile" clearable placeholder="手机号/姓名" class="filter-item" style="width:150px;"></el-input>
+            <el-input size="small" v-model="searchParams.mobile" clearable placeholder="公寓/小区-房间" class="filter-item" style="width:150px;"></el-input>
+            <el-button size="small" type="success" icon="el-icon-upload" class="filter-item">发布</el-button>
+            <el-button size="small" type="danger" icon="el-icon-remove" class="filter-item">撤销</el-button>
+          </div>
+        </el-form>
+        <GridUnit
+          ref="refGridUnit"
+          :columns="colModels"
+          :formOptions="searchParams"
+          :url="url"
+          :showSelection="true"
+          :pageSizes="[500, 1000, 2000, 3000, 5000]"
+          :dataMethod="method"
+          :height="tableHeight">
+        </GridUnit>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+import GridUnit from '@/components/GridUnit/grid'
+import { deepClone, cleanArray, ObjectMap } from '@/utils'
+import { hotRecommendApi, appIconApi } from '@/api/eeop'
+export default {
+  name: 'houseSync',
+  components: {
+    GridUnit
+  },
+  data() {
+    return {
+      tabMapOptions: ['整租','合租', '集中式'],
+      activeName: '整租',
+      searchParams: {
+        type: 2,
+        organizationName: '',
+        mobile: ''
+      },
+      colModels: [
+        { prop: 'status', label: '组织名称' },
+        { prop: 'status', label: '姓名', width: 100 },
+        { prop: 'status', label: '手机号', width: 150 },
+        { prop: 'status', label: '用户类型', width: 100 },
+        { prop: 'status', label: '房源位置', width: 200 },
+        { prop: 'status', label: '公寓/小区-房间', width: 200 },
+        { prop: 'status', label: '房源类型', width: 150 },
+        { prop: 'status', label: '房间状态', width: 100 },
+        { prop: 'status', label: '房源编码', width: 100 },
+        { prop: 'status', label: '房价(元/月)', width: 100, align: 'right' },
+        { prop: 'status', label: '麦邻租房', width: 100 },
+        { prop: 'status', label: '闲鱼租房', width: 100 },
+        { prop: 'status', label: '操作记录', width: 180 }
+      ],
+      tableHeight: 300,
+      url: hotRecommendApi.defaultOptions.requestUrl,
+      method: hotRecommendApi.defaultOptions.method
+    }
+  },
+  mounted() {
+    /* 表格高度控制 */
+   this.$nextTick(() => {
+      const offsetTop = 230
+      const pagenationH = 64
+      const containerPadding = 20
+      let temp_height = document.body.clientHeight - offsetTop - pagenationH - containerPadding
+      this.tableHeight = temp_height > 300 ? temp_height : 300
+      window.onresize = () => {
+        return (() => {
+          temp_height = document.body.clientHeight - offsetTop - pagenationH - containerPadding
+          this.tableHeight = this.tableHeight = temp_height > 300 ? temp_height : 300
+        })()
+      }
+    })
+  },
+  computed: {
+    tableStyle: function() {
+      return {
+        width: '100%',
+        height: this.tableHeight + 'px'
+      }
+    }
+  },
+  methods: {
+    // 查询
+    searchParam(type) {
+      if (type === 'clear') {
+        this.searchParams = {}
+      }
+      this.$refs.refGridUnit[0].searchHandler()
+    }
+  }
+}
+</script>
+<style rel="stylesheet/scss" lang="scss">
+.model-search .filter-item {
+  margin-left: 10px;
+}
+
+.item-select {
+  width: 150px;
+}
+</style>
