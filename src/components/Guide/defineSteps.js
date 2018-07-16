e8f9a93c42d693f30fab48c83514bf403450fd06
王玲波 <454949876@qq.com>
2018-07-16 14:19:13 +0800

    ### *柏林* 新增guide引导组件

---
+++ src/components/Guide/defineSteps.js
@@ -0,0 +1,68 @@
+const steps = [
+  {
+    element: '.sidebar-wrapper',
+    popover: {
+      title: '侧边栏导航',
+      description: '这里是所有你能访问的菜单，随便点点试试吧！',
+      position: 'right'
+    }
+  },
+  {
+    element: '.hamburger-container',
+    popover: {
+      title: '侧边栏开关',
+      description: '打开/关闭侧边栏导航，更好的浏览页面信息哦~',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.tags-view-container',
+    popover: {
+      title: '标签视图',
+      description: '记录你所有打开过的页面，可右键操作~',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.guide',
+    popover: {
+      title: '新手引导',
+      description: '这东东就是你现在正在用的，点击即可打开新手引导',
+      position: 'left'
+    }
+  },
+  {
+    element: '.screenfull',
+    popover: {
+      title: '全屏',
+      description: '全屏浏览页面，极致体验！',
+      position: 'left'
+    }
+  },
+  {
+    element: '.theme-switch',
+    popover: {
+      title: '换肤',
+      description: '自定义皮肤，穿什么衣服由你决定，想不想尝试一下？',
+      position: 'left'
+    }
+  },
+  {
+    element: '.avatar-container',
+    popover: {
+      title: '个人信息',
+      description: '在这里你可以查看个人信息，也可以快速回到首页、退出系统',
+      position: 'left'
+    }
+  },
+  {
+    element: '.notification',
+    popover: {
+      title: '审核消息',
+      description: '这里推送房源审核消息，点击即可跳转到相应页面进行审核哦',
+      position: 'left'
+    }
+  }
+]
+
+export default steps
---
+++ src/components/Guide/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="guide-icon" @click.prevent.stop="guide">
+    <icon-svg icon-class="guide" />
+  </div>
+</template>
+
+<script>
+import * as Driver from 'driver.js'
+import 'driver.js/dist/driver.min.css'
+import steps from './defineSteps'
+
+export default {
+  name: 'guide',
+  data() {
+    return {
+      driver: null
+    }
+  },
+  mounted() {
+    this.driver = new Driver({
+      doneBtnText: '结束引导',
+      closeBtnText: '关闭',
+      stageBackground: '#fff',
+      nextBtnText: '下一个',
+      prevBtnText: '上一个'
+    })
+  },
+  methods: {
+    guide() {
+      this.driver.defineSteps(steps)
+      this.driver.start()
+    }
+  }
+}
+</script>
+<style scoped>
+.guide-icon {
+  display: inline-block;
+  cursor: pointer;
+  color: #f56c6c;
+  font-size: 40px;
+  vertical-align: 6px;
+}
+</style>
