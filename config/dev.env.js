1ad6cb0fc265ea0bd923a045606e94584577f132
weichen12 <2227538634@qq.com>
2018-08-24 17:54:56 +0800

    冲突

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,12 +2,7 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   NPM_CONFIG_REPORT: '"false"',
-<<<<<<< HEAD
-  BASE_API: '"https://pre.mdguanjia.com/bop/"',
-  //  BASE_API: '"http://0.0.0.0:9528/api/"',
-=======
-   BASE_API: '"https://dev.mdguanjia.com/bop/"',
+  BASE_API: '"https://dev.mdguanjia.com/bop/"',
     // BASE_API: '"http://localhost:9528/api/"',
->>>>>>> ghost
   FLY_API: '"https://test-flying-api.mdguanjia.com"'
 }
