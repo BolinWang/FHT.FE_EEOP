362ace8b195498ab72dd821e1f3a4cfe837421ec
褚德权 <chudequan_48@163.com>
2018-09-14 14:36:00 +0800

    分散式录房代码提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   NPM_CONFIG_REPORT: '"false"',
-  BASE_API: '"https://dev.mdguanjia.com/bop/"',
+  BASE_API: '"https://test1.mdguanjia.com/bop/"',
   // BASE_API: '"http://localhost:9528/api/"',
   FLY_API: '"https://test-flying-api.mdguanjia.com"'
 }
